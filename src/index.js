0c3b6e3827bdf2d23a2a3f5a63417ed5f9748ce1
Denys-Leonov <skydark912@gmail.com>
2022-07-17 15:11:56 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 
 
 ReactDOM.render(
-  <App />, 
+  <div>
+    the app is working!
+  </div>, 
   document.getElementById('root')
 )
 
