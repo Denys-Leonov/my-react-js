fc2d47de88f531d108280a8da441369b219666f4
Denys-Leonov <skydark912@gmail.com>
2022-07-19 01:13:30 +0300

    42:05

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react'
-import ClassCounter from './components/ClassCounter'
-// import Counter from './components/Counter'
+import PostItem from './components/PostItem'
+import PostList from './components/PostList'
+import './styles/App.css'
+
 function App() {
-  const [value, setValue] = useState('Some text')
 
-
-
+  const arr = [
+    {
+      id: 1,
+      title: 'Javascript',
+      body: 'Javascript is a programming language #1',
+    },
+    { id: 2, title: 'Python', body: 'Javascript is a programming language #2' },
+    { id: 3, title: 'C+', body: 'Javascript is a programming language #3' },
+    { id: 4, title: 'C++', body: 'Javascript is a programming language #4' },
+    { id: 5, title: 'C#', body: 'Javascript is a programming language #5' },
+    { id: 6, title: 'Java', body: 'Javascript is a programming language #6' },
+  ]
+  
+  
   return (
     <div className="App">
-     <ClassCounter />
+      <PostList posts={arr} title='List of posts #1'/>
     </div>
   )
 }
