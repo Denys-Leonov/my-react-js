7251d1ad9a7ac728bf76fea97cd2af55a9514c32
Denys-Leonov <skydark912@gmail.com>
2022-07-25 17:23:56 +0300

    useMemo for Selecting + Search bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import PostForm from './components/PostForm'
 // import PostItem from './components/PostItem'
 import PostList from './components/PostList'
+import MyInput from './components/UI/input/MyInput'
 import MySelect from './components/UI/select/MySelect'
 import './styles/App.css'
 
@@ -20,19 +21,19 @@ function App() {
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const sortedPosts = [...posts].sort((a, b) => a[sort].localeCompare(b[sort]))
 
   function sortPosts(sort) {
-    
     setSelectedSort(sort)
-    console.log(sort)
-    setPosts([...posts].sort((a,b) =>  a[sort].localeCompare(b[sort])))
   }
 
   function createPost(newPost) {
     setPosts([...posts, newPost])
   }
 
-  function removePost(post) {
+  function removePost(post) { 
     setPosts(posts.filter((p) => p.id !== post.id))
   }
 
@@ -42,6 +43,8 @@ function App() {
       <hr
         style={{ margin: '15px 0px', border: '1px solid rgb(2, 254, 216)' }}
       />
+
+      <MyInput placeholder="Searching..." value={searchQuery} onChange={e => setSearchQuery(e.target.value)}/>
       <MySelect
         value={selectedSort}
         onChange={sortPosts}
